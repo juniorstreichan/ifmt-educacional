51064e2c8ee00aa7974e6b06e1e1f003dadeb63b
Robson Pereira Braz Junior <robsonbrazjunior@gmail.com>
2018-12-03 16:09:28 -0300

    Criada classe AtividadeVO

---
+++ src/ssatividade/vo/AtividadeVO.java
@@ -0,0 +1,47 @@
+package ssatividade.vo;
+
+import base.vo.BaseVO;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import ssprofessor.vo.ProfessorVO;
+
+@Embeddable
+public class AtividadeVO extends BaseVO {
+
+    @Column
+    private String periodoLetivo;
+    
+    private ProfessorVO professor;
+
+    public String getPeriodoLetivo() {
+        return periodoLetivo;
+    }
+
+    public void setPeriodoLetivo(String periodoLetivo) {
+        this.periodoLetivo = periodoLetivo;
+    }
+
+    public ProfessorVO getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(ProfessorVO professor) {
+        this.professor = professor;
+    }
+    
+    @Override
+    public String toString() {
+        return this.periodoLetivo + ", " + this.professor;
+    }
+
+    @Override
+    public boolean isValido() {
+       boolean resp = true;
+       
+       if (this.periodoLetivo == null || this.periodoLetivo.length() == 0 || this.periodoLetivo.length() > 100) {
+            this.validacaoMsg += "Período Letivo inválido";
+            resp = false;
+        }
+        return resp;
+    }    
+}
