fe7bb52865bb4631a971a688a6d2e66207fe2efc
Lucas Costa Campos <lucasnick33@gmail.com>
2018-12-03 22:17:13 -0300

    Negocio e Servico

---
+++ api-base/src/java/servico/Servico.java
@@ -0,0 +1,62 @@
+package servico;
+
+import base.negocio.Negocio;
+import base.negocio.RetornoNegocio;
+import base.vo.EntidadeVO;
+import com.google.gson.Gson;
+
+public abstract class Servico<VO extends EntidadeVO> implements IServico {
+
+    private final Negocio<VO> negocio;
+    private final Class classe;
+    private final Gson gson = new Gson();
+    
+    public Servico(Negocio<VO> negocio, Class<VO> classe) {
+        this.negocio = negocio;
+        this.classe = classe;
+    }
+    
+    @Override
+    public String cadastrar(String strObjeto) {
+        VO vo = (VO) gson.fromJson(strObjeto, classe);
+        RetornoNegocio retorno = negocio.validarInclusao(vo);
+        if (!retorno.isValido()) {
+            return gson.toJson(retorno);
+        }
+        return gson.toJson(new RetornoNegocio(true, vo.toString() + " cadastrado com sucesso!"));
+    }
+
+    @Override
+    public String alterar(String strObjeto) {
+        VO vo = (VO) gson.fromJson(strObjeto, classe);
+        RetornoNegocio retorno = negocio.validarAlteracao(vo);
+        if (!retorno.isValido()) {
+            return gson.toJson(retorno);
+        }
+        return gson.toJson(new RetornoNegocio(true, vo.toString() + " alterado com sucesso!"));
+    }
+
+    @Override
+    public String remover(String strObjeto) {
+        VO vo = (VO) gson.fromJson(strObjeto, classe);
+        RetornoNegocio retorno = negocio.validarExclusao(vo);
+        if (!retorno.isValido()) {
+            return gson.toJson(retorno);
+        }
+        return gson.toJson(new RetornoNegocio(true, vo.toString() + " excluído com sucesso!"));
+    }
+
+    @Override
+    public String buscarPorId(String strId) {
+        RetornoNegocio retorno = negocio.validarBuscaPorId(strId);
+        if (!retorno.isValido()) {
+            return gson.toJson(retorno);
+        }
+        VO vo = negocio.buscarPorId(Long.parseLong(strId));
+        if (vo == null) {
+            return gson.toJson(new RetornoNegocio(false, "Nenhum resultado encontrado para o ID " + strId));
+        }
+        return gson.toJson(vo);
+    }
+    
+}
--- projeto-base/src/base/negocio/Negocio.java
+++ projeto-base/src/base/negocio/Negocio.java
@@ -1,9 +1,16 @@
 package base.negocio;
 
 import base.vo.EntidadeVO;
+import java.util.List;
 
-public class Negocio<VO extends EntidadeVO> implements INegocio<VO> {
+public abstract class Negocio<VO extends EntidadeVO> implements INegocio<VO> {
 
+    List<VO> listaVO;
+    
+    public Negocio(List<VO> listaVO) {
+        this.listaVO = listaVO;
+    }
+    
     @Override
     public RetornoNegocio validarInclusao(VO vo) {
         if (vo == null) {
@@ -37,7 +44,28 @@ public class Negocio<VO extends EntidadeVO> implements INegocio<VO> {
         if (vo.getId() == null) {
             return new RetornoNegocio(false, "O identificador dos dados está vazio!");
         }
-        return new RetornoNegocio(true, "Dados Válidos para a Alteração");
+        return new RetornoNegocio(true, "Dados Válidos para a Exclusão");
+    }
+    
+    public RetornoNegocio validarBuscaPorId(String strId) {
+        if (strId == null || strId.trim().isEmpty()) {
+            return new RetornoNegocio(false, "Dados inconsistentes - elemento vazio");
+        }
+        try {
+            Long.parseLong(strId);
+        } catch(NumberFormatException ex) {
+            return new RetornoNegocio(false, "Dados inconsistentes - o identificador não é um número inteiro!");
+        }
+        return new RetornoNegocio(true, "Dados Válidos para a Busca por ID");
+    }
+    
+    public VO buscarPorId(Long id) {
+        for (VO vo : listaVO) {
+            if (vo.getId().equals(id)) {
+                return vo;
+            }
+        }
+        return null;
     }
     
 }
